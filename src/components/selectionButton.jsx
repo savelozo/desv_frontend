617e65902048a63e1f938902407d0d5ab1643ad6
savelozo <savelozo@uc.cl>
2023-08-17 20:57:12 -0400

    third try to fix keys

--- src/components/selectionButton.jsx
+++ src/components/selectionButton.jsx
@@ -8,6 +8,7 @@ const SelectionButton = ({ imgBtn, buttonLabel }) => {
 
   return (
     <button
+        key={imgBtn - 1}
         className="flex items-center justify-center bg-white hover:bg-slate-300 text-sky-600 font-semibold py-2 px-4 rounded">
         <div className="flex flex-col items-center">        
             {svg_list[imgBtn - 1]}
