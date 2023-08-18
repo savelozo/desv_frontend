0443c425b4261df26a6c59b32eb8a15c5710c5b8
savelozo <savelozo@uc.cl>
2023-08-17 20:02:49 -0400

    Add selection buttons and icons

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React, { useState } from 'react';
+import ButtonGrid from '../components/buttonGrid';
+
+const Home_page = () => {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <ButtonGrid />
+    </div>  
+  );
+};
+
+export default Home_page;
---
+++ src/components/selectionButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HomeSvg, TruckSvg, ProjectSvg, BankNotesSvg, ChartBarSvg, CurrencySvg, CreditCardSvg, StoreFrontSvg } from './svgs';
+
+
+const SelectionButton = ({ buttonKey, buttonLabel }) => {
+
+    const svg_list = [<HomeSvg/>,<TruckSvg />,<ProjectSvg />,<BankNotesSvg/>,<ChartBarSvg/>,<CurrencySvg/>,<CreditCardSvg/>,<StoreFrontSvg/>]
+
+  return (
+    <button
+        key={buttonKey}
+        className="flex items-center justify-center bg-white hover:bg-slate-300 text-sky-600 font-semibold py-2 px-4 rounded">
+        <div className="flex flex-col items-center">        
+            {svg_list[buttonKey - 1]}
+            <span className="mt-2">{buttonLabel}</span>
+        </div>
+    </button>
+  );
+};
+
+
+
+export default SelectionButton;
