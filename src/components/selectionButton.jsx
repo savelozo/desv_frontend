1ccd6798e56fce82020dc162a62333777ac502b3
savelozo <savelozo@uc.cl>
2023-08-17 21:39:13 -0400

    F5

--- src/components/selectionButton.jsx
+++ src/components/selectionButton.jsx
@@ -8,9 +8,9 @@ const SelectionButton = ({ imgBtn, buttonLabel }) => {
     <button
         key={imgBtn - 1}
         className="flex items-center justify-center bg-white hover:bg-slate-300 text-sky-600 font-semibold py-2 px-4 rounded">
-        <div className="flex flex-col items-center" key={imgBtn - 1}>        
+        <div className="flex flex-col items-center">        
             {svg_list[imgBtn - 1]}
-            <span className="mt-2" key={imgBtn - 1}>{buttonLabel}</span>
+            <span className="mt-2">{buttonLabel}</span>
         </div>
     </button>
   );
